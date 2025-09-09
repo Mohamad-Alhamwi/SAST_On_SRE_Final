d32f8dcb305a21912f8d409cb3c75c44a463e0c1
49732230+Mohamad-Alhamwi@users.noreply.github.com <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2025-09-09 14:20:59 -0400

    Add parser scripts.

---
+++ Scripts/cppcheck_parser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+from collections import Counter
+
+log_file = sys.argv[1]
+bugs = []
+
+with open(log_file, "r") as logs:
+    for line in logs:
+        match = re.search(r"\.\s\[([^\]]+)\]", line)
+
+        if match:
+            bugs.append(match.group(1))
+
+bug_types = Counter(bugs)
+
+print("\nErrors List:\n")
+
+max_len = max(len(bug) for bug in bug_types)
+
+for bug, count in bug_types.items():
+    print(f"{bug:<{max_len}} : {count}")
